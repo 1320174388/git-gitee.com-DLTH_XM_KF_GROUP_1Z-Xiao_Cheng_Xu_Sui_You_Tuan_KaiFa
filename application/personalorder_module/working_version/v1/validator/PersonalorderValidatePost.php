c155621e0b5786e827f68410547c30e65f3e1182
jingzhiqiang <jingzhiqiang@dlaotianhuang.com>
2018-10-10 23:36:50 +0800

    查个人订单

---
+++ application/personalorder_module/working_version/v1/validator/PersonalorderValidatePost.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  PersonalorderValidatePost.php
+ *  创 建 者 :  Jing Zhi Qiang
+ *  创建日期 :  2018/10/10 19:29
+ *  文件描述 :  个人订单添加验证器
+ *  历史记录 :  -----------------------
+ */
+namespace app\personalorder_module\working_version\v1\validator;
+use think\Validate;
+
+class PersonalorderValidatePost extends Validate
+{}
