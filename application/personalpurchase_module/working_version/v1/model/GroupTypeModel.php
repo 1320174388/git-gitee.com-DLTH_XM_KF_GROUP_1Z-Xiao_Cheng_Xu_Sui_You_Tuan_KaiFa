87fb3b53282aebdc5eafbabf75ef3d6d2d736cc2
时广玉 <shiguangyu@dlaotianhuang.com>
2018-10-15 14:49:47 +0800

    2018/10/15 14:05 更新项目~支付模块更新

--- application/personalpurchase_module/working_version/v1/model/GroupTypeModel.php
+++ application/personalpurchase_module/working_version/v1/model/GroupTypeModel.php
@@ -21,6 +21,6 @@ class GroupTypeModel extends Model
     // 加载配置数据表名
     protected function initialize()
     {
-        $this->table = config('v1_tableName.SGY_Group_list');
+        $this->table = config('wx_sgy_config.SGY_Group_list');
     }
 }
